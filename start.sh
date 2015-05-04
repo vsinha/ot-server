badb556556d59b1e904c26ad997287ea3a9af4d0
Viraj Sinha <virajosinha@gmail.com>
2015-05-04 19:28:39 +0000

    fixed missing component path bug

--- start.sh
+++ start.sh
@@ -3,6 +3,7 @@
 source /usr/local/bin/virtualenvwrapper.sh
 
 workon ot2
+#crossbar start > crossbar.log 2>&1 &
 crossbar start &
 deactivate
 
@@ -12,3 +13,4 @@ sleep 20s
 
 workon ot3
 python ot_client/client.py
+# continue on in ot3 virtualenv
